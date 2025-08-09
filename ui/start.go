07c741024a146f9eb3c981623969da058d1fcc3e
this-nightowl <ian@nightowlabs.com>
2025-08-09 00:16:17 -0600

    Revamp UI with dark theme, header/footer, tooltips

--- ui/start.go
+++ ui/start.go
@@ -4,6 +4,6 @@ import tea "github.com/charmbracelet/bubbletea"
 
 // Start launches the Bubble Tea program.
 func Start() error {
-	p := tea.NewProgram(initialModel())
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
 	return p.Start()
 }
